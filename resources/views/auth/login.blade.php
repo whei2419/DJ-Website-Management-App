f7a41a32a9e14900379974fdc7efa3032e226a3a
whei2419 <87625823+whei2419@users.noreply.github.com>
2026-01-15 09:51:05 +0800

    update style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,19 +5,23 @@
 @section('content')
 <div class="page page-center">
     <div class="container-tight py-4">
-        <div class="text-center mb-5">
+        <div class="text-center mb-5" style="margin-top: 50px;">
             <a href="/" class="navbar-brand navbar-brand-autodark">
                 <img src="{{ URL::asset('assets/images/logo.webp') }}" height="48" alt="Your Logo">
             </a>
         </div>
-        <div class="card card-md">
+        <div class="card card-md shadow-sm border-0">
             <div class="card-body">
-                <h2 class="h2 text-center mb-4">{{ __('Sign in to your account') }}</h2>
+                <h2 class="h2 text-center mb-4">
+                    <i class="fa-solid fa-user-lock"></i> {{ __('Sign in to your account') }}
+                </h2>
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
 
                     <div class="mb-3">
-                        <label class="form-label">{{ __('Email address') }}</label>
+                        <label class="form-label">
+                            <i class="fa-solid fa-envelope"></i> {{ __('Email address') }}
+                        </label>
                         <input type="email" class="form-control" name="email" placeholder="Enter email" value="{{ old('email') }}" required autofocus>
                         @error('email')
                             <span class="text-danger">{{ $message }}</span>
@@ -26,7 +30,7 @@
 
                     <div class="mb-2">
                         <label class="form-label">
-                            {{ __('Password') }}
+                            <i class="fa-solid fa-lock"></i> {{ __('Password') }}
                             @if (Route::has('password.request'))
                                 <span class="form-label-description">
                                     <a href="{{ route('password.request') }}">{{ __('Forgot Password?') }}</a>
@@ -47,13 +51,12 @@
                     </div>
 
                     <div class="form-footer">
-                        <button type="submit" class="btn btn-primary w-100">{{ __('Sign in') }}</button>
+                        <button type="submit" class="btn btn-primary w-100">
+                            <i class="fa-solid fa-sign-in-alt"></i> {{ __('Sign in') }}
+                        </button>
                     </div>
                 </form>
             </div>
-        </div>
-        <div class="text-center text-muted mt-3">
-            {{ __('Don\'t have an account?') }} <a href="{{ route('register') }}" tabindex="-1">{{ __('Sign up') }}</a>
         </div>
     </div>
 </div>
