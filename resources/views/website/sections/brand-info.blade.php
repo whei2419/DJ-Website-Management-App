de858810a41884525fc5459e6d2c220562d97b73
whei2419 <87625823+whei2419@users.noreply.github.com>
2026-01-28 16:18:46 +0800

    change layout

--- resources/views/website/sections/brand-info.blade.php
+++ resources/views/website/sections/brand-info.blade.php
@@ -8,36 +8,39 @@
 
 <div class="container-fluid section-brand">
     <div class="container text-center py-4">
-        <h1 data-aos="fade-down" data-aos-duration="800">IGNITE THE NIGHT:
+        <h1 data-aos="fade-down" data-aos-duration="800">YSL BEAUTY LIGHT CLUB
         </h1>
-        <h1 data-aos="fade-down" data-aos-duration="800" data-aos-delay="200">YSL BEAUTY LIGHT CLUB RISING BEATS</h1>
+        <h1 data-aos="fade-down" data-aos-duration="800" data-aos-delay="200">RISING BEATS</h1>
         <p data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
-            STEP INTO THE SPOTLIGHT
-            MALAYSIA’S ULTIMATE UP & COMING DJ SHOWCASE PLATFORM BRING YOUR USB AND TAKE OVER OUR SOUND SYSTEM
+            BECOME THE ICON. RISE TO YOUR OWN BEAT.
         </p>
     </div>
 </div>
 
 <div class="container-fluid section-register">
     <div class="container text-center">
-        <p data-aos="fade-up" data-aos-duration="700">UNLEASH YOUR SOUND AND ELECTRIFY THE CROWD AT YSL BEAUTY LIGHT
-            CLUB RISING BEATS, THE STAGE WHERE FRESH
-            TALENT TAKES CENTER STAGE. WHETHER YOU’RE A SEASONED DJ OR A FIRST-TIME PERFORMER, THIS IS YOUR MOMENT TO
-            SHINE. NO COMPETITION, NO PRESSURE, JUST PURE OPPORTUNITY.
+        <p data-aos="fade-up" data-aos-duration="700">
+            YSL BEAUTY INVITES THE NEXT GENERATION OF SOUND-MAKERS TO SEIZE THE BOOTH AND REDEFINE THE ATMOSPHERE​​AT
+            THE YSL BEAUTY LIGHT CLUB POP-UP
         </p>
 
         <p data-aos="fade-up" data-aos-duration="700" data-aos-delay="100">
-            JOIN US AT THE YSL BEAUTY LIGHT CLUB POP-UP AT THE EXCHANGE TRX, PLUG IN YOUR USB, AND DELIVER YOUR MOST
-            CAPTIVATING SET. EACH PERFORMANCE WILL BE PROFESSIONALLY RECORDED AND FEATURED ON OUR EXCLUSIVE WEBSITE,
-            GIVING YOU THE EXPOSURE YOU DESERVE. IT’S MORE THAN AN EVENT - IT’S A CELEBRATION OF CREATIVITY, COMMUNITY,
-            AND THE NEXT WAVE OF MALAYSIAN MUSIC CULTURE.
+            PLUG IN. PLAY. RISE WITH THE BEAT.
         </p>
 
-        <p data-aos="fade-up" data-aos-duration="700" data-aos-delay="200">
-            READY TO MAKE YOUR MARK? RESERVE YOUR SPOT AND BE PART OF THE MOVEMENT.
+        <div class="devider" data-aos="fade-up" data-aos-duration="700" data-aos-delay="200"></div>
+
+        <p class="px-5" data-aos="fade-up" data-aos-duration="700" data-aos-delay="300">
+            THE EXCHANGE TRX
+            G FLOOR, INFRONT OF CONCIERGE
+            7, 8, 14, 15, 21, 22, 28 FEB & 1 MAR 2026
+        </p>s
+
+        <p data-aos="fade-up" data-aos-delay="400">
+            THE DECKS ARE WAITING. THE SPOTLIGHT IS YOURS.​​
         </p>
 
-        <div class="button-container" data-aos="fade-up" data-aos-duration="800" data-aos-delay="300">
+        <div class="button-container" data-aos="fade-up" data-aos-duration="800" data-aos-delay="500">
             <a href="https://outlook.office.com/book/YSLBEAUTYLIGHTCLUBRISINGBEATS@loreal.onmicrosoft.com/?ismsaljsauthenabled"
                 class="btn btn-primary" target="_blank" rel="noopener">REGISTER NOW</a>
             <a href="{{ route('site.faq') }}" class="btn btn-primary">FAQ</a>
--- resources/views/website/sections/cards.blade.php
+++ resources/views/website/sections/cards.blade.php
@@ -1,44 +1,78 @@
 <div id="howToParticipate" class="container-fluid section-with-cards">
-    <div class="row m-0 p-0">
-        <div class="col-md-4 col-12" data-aos="fade-up" data-aos-duration="800">
+
+
+    <div class="row m-0 p-0 cards-row">
+        <div class="col-md-4 col-12 card-col" data-aos="fade-up" data-aos-duration="800">
             <div class="content-container">
                 <p class="number">01</p>
-                <h1 class="title">
-                    Enter
-                </h1>
-                <p class="description">
-                    REGISTER HERE AND CLAIM YOUR SPOT AT YSL BEAUTY LIGHT CLUB RISING BEATS. NO PROFESSIONAL EXPERIENCE
-                    REQUIRED, JUST PASSION AND A DESIRE TO SHARE YOUR SOUND. ALL MALAYSIANS AGED 18+ ARE WELCOME TO STEP
-                    INTO THE SPOTLIGHT. IT’S YOUR CHANCE TO BE HEARD, SEEN, AND CELEBRATED.
-                </p>
+                <h1 class="title">SUBMIT</h1>
+                <p class="description">SHARE YOUR DJ PROFILE (IG, TIKTOK, OR YOUTUBE) HERE. WE'LL REVIEW AND CONFIRM YOUR
+                    SPOT. BRING YOUR PASSION AND DESIRE TO SHARE YOUR SOUND. MALAYSIANS AGED 18+ ARE WELCOME TO STEP
+                    INTO THE SPOTLIGHT.</p>
             </div>
         </div>
-        <div class="col-md-4 col-12 white" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
+
+        <div class="col-md-4 col-12 white card-col" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
             <div class="content-container">
                 <p class="number">02</p>
-                <h1 class="title">
-                    PERFORM
-                </h1>
-                <p class="description">
-                    TAKE THE STAGE AT YSL BEAUTY LIGHT CLUB POP-UP AT THE EXCHANGE TRX (G FLOOR, INFRONT OF CONCIERGE)
-                    AND UNLEASH YOUR SET. WHETHER YOU’RE DROPPING YOUR FIRST BEAT OR YOUR HUNDREDTH, THIS IS YOUR
-                    MOMENT. PLUG IN, PLAY AT LEAST FIVE TRACKS, AND LET YOUR ENERGY IGNITE THE CROWD. ALL PERFORMANCES
-                    ARE PROFESSIONALLY RECORDED FOR MAXIMUM IMPACT..
-                </p>
+                <h1 class="title">PLUG-IN</h1>
+                <p class="description">WE PROVIDE CDJ3000 AND DJM900 NEXUS2 FOR YOUR SET. JUST BRING YOUR USB,
+                    HEADPHONES, AND DJ SOFTWARE &amp; LAPTOP. BE READY TO SHOW UP, SHARE YOUR SOUND, BECOME THE ICON,
+                    RISE TO YOUR OWN BEAT.</p>
             </div>
         </div>
-        <div class="col-md-4 col-12" data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
+
+        <div class="col-md-4 col-12 card-col" data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
             <div class="content-container">
                 <p class="number">03</p>
-                <h1 class="title">
-                    SHINE
-                </h1>
-                <p class="description">
-                    YOUR SET DOESN’T END WHEN THE MUSIC STOPS. EVERY PERFORMANCE IS UPLOADED TO OUR EXCLUSIVE WEBSITE.
-                    GIVING YOU EXPOSURE TO FANS, TASTEMAKERS, AND THE MUSIC COMMUNITY. SHARE YOUR SPOTLIGHT, INSPIRE
-                    OTHERS, AND BECOME PART OF MALAYSIA’S NEXT WAVE OF SOUND.
-                </p>
+                <h1 class="title">PLAY</h1>
+                <p class="description">TAKE THE STAGE AT THE YSL BEAUTY LIGHT CLUB POP-UP AND UNLEASH YOUR SET. THIS IS
+                    YOUR MOMENT. PLUG IN, PLAY AT LEAST FIVE TRACKS, AND IGNITE THE CROWD DURING YOUR 30-MINUTE
+                    PERFORMANCE. ALL SETS ARE PROFESSIONALLY RECORDED FOR MAXIMUM IMPACT.</p>
             </div>
         </div>
+
+        <div class="col-md-4 col-12 white card-col" data-aos="fade-up" data-aos-duration="800" data-aos-delay="600">
+            <div class="content-container">
+                <p class="number">04</p>
+                <h1 class="title">ASCEND</h1>
+                <p class="description">LEAVE YOUR MARK AT THE YSL BEAUTY LIGHT CLUB. YOUR PERFORMANCE WILL BE
+                    IMMORTALISED AND SHARED ON THIS WEBSITE FOR THE WORLD TO HEAR.</p>
+            </div>
+        </div>
+
+        <div class="col-md-4 col-12 card-col" data-aos="fade-up" data-aos-duration="800" data-aos-delay="800">
+            <div class="content-container">
+                <p class="number">05</p>
+                <h1 class="title">REWARD</h1>
+                <p class="description">SHOW OFF YOUR SOUND WITH A BESPOKE YSL HIGHLIGHT REEL TO SHARE AND TAG
+                    #YSLBEAUTYMY, THEN WALK AWAY WITH AN EXCLUSIVE YSL BEAUTY KIT.</p>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+<div class="container-fluid section-gif">
+    <img src="{{ asset('assets/images/Gif_Landscape.gif') }}" alt="">
+</div>
+
+
+<div class="container-fluid section-white-2 bg-white">
+    <div class="container text-center py-4" data-aos="fade-up" data-aos-duration="800">
+        <h1>NOT DROPPING BEATS?
+            DISCOVER THE YSL BEAUTY LIGHT CLUB EXPERIENCE</h1>
+
+        <p>STEP INTO THE YSL BEAUTY LIGHT CLUB POP-UP – WHERE LIGHTS, SOUND, AND ICONIC COUTURE BEAUTY TAKE OVER.
+            PRE-REGISTER NOW TO REDEEM YOUR EXCLUSIVE GIFT* AT THE VENUE WHEN YOU VISIT.</p>
+
+        <p>THE EXCHANGE TRX
+            (G FLOOR, IN FRONT OF CONCIERGE)</p>
+
+        <p>3 FEB – 1 MAR</p>
+
+        <small>*TERMS & CONDITIONS APPLY. APPLICABLE FOR REDEMPTION WHILE STOCKS LAST, UPON COMPLETION OF JOURNEY AT THE
+            VENUE.</small>
     </div>
 </div>
